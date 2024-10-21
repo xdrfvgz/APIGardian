268a86c0e968a99c909bcd640b71dcc9baa79169
xdrfvgz <57246383+xdrfvgz@users.noreply.github.com>
2024-10-21 14:09:18 +0200

    Update install.sh

--- system_setup/install.sh
+++ system_setup/install.sh
@@ -6,8 +6,8 @@ bash ./restore_apt_packages.sh
 echo ""
 echo "create venv:"
 echo ""
-python -m venv ~/apk
-source ~/apk/bin/activate
+python -m venv apk
+source apk/bin/activate
 
 echo ""
 echo "Install pip packeges:"
