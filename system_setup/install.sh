844ef52a8723e016ab1bd6eadc95a7112dd2d19d
Ubuntu <ubuntu@ip-172-31-7-171.eu-north-1.compute.internal>
2024-10-22 14:22:09 +0000

    app wird dargestellt

--- system_setup/install.sh
+++ system_setup/install.sh
@@ -3,13 +3,13 @@ echo "Install apt packeges:"
 echo ""
 bash ./restore_apt_packages.sh
 
-echo ""
-echo "create venv:"
-echo ""
-python -m venv apk
-source apk/bin/activate
+#echo ""
+#echo "create venv:"
+#echo ""
+#python -m venv apk
+#source apk/bin/activate
 
-echo ""
-echo "Install pip packeges:"
-echo ""
-bash ./restore_pip_packages.sh
+#echo ""
+#echo "Install pip packeges:"
+#echo ""
+#bash ./restore_pip_packages.sh
